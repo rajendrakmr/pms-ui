b10c57d04b0b3a445c797d07ff3b0b56a14ba2ec
cloudwithrk <cloudwithrk@gmail.com>
2026-02-13 14:03:01 +0530

    update menu

--- src/store/apiSlice.ts
+++ src/store/apiSlice.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "./index";
+
 import {
   ENV,
   DEV_API_URL,
--- src/store/slice/menuSlice.ts
+++ src/store/slice/menuSlice.ts
@@ -17,10 +17,102 @@ const menuSlice = createSlice({
         menuNameTree: "Dashboard",
         menuLinkName: "/dashboard",
       };
+      const items = [
+        {
+          menuNameTree: "Dashboard",
+          menuLinkName: "/dashboard"
+        },
+        {
+          menuNameTree: "Security",
+          menuLinkName: "/security",
+          children: [
+            {
+              menuNameTree: "Transaction",
+              menuLinkName: "/security/transaction",
+              children: [
+                {
+                  menuNameTree: "User Access",
+                  menuLinkName: "/security/transaction/user-access",
+                  children: [
+                    { menuNameTree: "Add", menuLinkName: "/addUserAccess" },
+                    { menuNameTree: "Edit", menuLinkName: "/editUserAccess" }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          menuNameTree: "Agent",
+          menuLinkName: "/security",
+          children: [
+            {
+              menuNameTree: "Application",
+              menuLinkName: "/security/transaction",
+              children: [
+                {
+                  menuNameTree: "Ducument Upload",
+                  menuLinkName: "/security/transaction/user-access",
+                  children: [
+                    { menuNameTree: "View", menuLinkName: "/security/transaction/user-access/add" },
+                    { menuNameTree: "Edit", menuLinkName: "/security/transaction/user-access/edit" }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+
+        {
+          menuNameTree: "Container Operation",
+          menuLinkName: "/security",
+          children: [
+            {
+              menuNameTree: "Transaction",
+              menuLinkName: "/security/transaction",
+              children: [
+                {
+                  menuNameTree: "Gain In - Container",
+                  menuLinkName: "/security/transaction/user-access",
+                  children: [
+                    { menuNameTree: "Add", menuLinkName: "/addGateIn" },
+                    { menuNameTree: "Edit", menuLinkName: "/editGateIn" }
+                  ]
+                },
+                {
+                  menuNameTree: "Direct Port Entry Service Charges",
+                  menuLinkName: "/security/transaction/user-access",
+                  children: [
+                    { menuNameTree: "Add", menuLinkName: "/addDpeServiceCharge" },
+                    { menuNameTree: "Edit", menuLinkName: "/editDpeServiceCharge" }
+                  ]
+                },
+                {
+                  menuNameTree: "Document Upload",
+                  menuLinkName: "/security/transaction/user-access",
+                  children: [
+                    { menuNameTree: "View", menuLinkName: "/viewDocUpload" },
+                  ]
+                },
+                {
+                  menuNameTree: "Gain Out - Container",
+                  menuLinkName: "/security/transaction/user-access",
+                  children: [
+                    { menuNameTree: "Add", menuLinkName: "/addGateOut" },
+                    { menuNameTree: "Edit", menuLinkName: "/editGateOut" }
+                  ]
+                }
+
+              ]
+            }
+          ]
+        },
+
+      ]
 
       return {
-        items: [dashboardItem, ...action.payload],
-      }; 
+        items: items,
+      };
     },
     clearMenu: (state) => {
       state.items = [];
